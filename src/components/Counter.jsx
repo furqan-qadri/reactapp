a85de2f9fc47aa60215e5ec0d36743898d62e94b
Furqan Qadri <32431583+furqan-qadri@users.noreply.github.com>
2024-02-16 23:57:19 +0800

    add counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import {useState} from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Counter() {
-  const [count,setCount]=useState(0);
+  const [count, setCount] = useState(0);
 
   return (
-    <div>The count is: {count} </div>
-  )
+    <>
+      <div>The count is: {count} </div>
+    <button onClick={() => setCount(count + 1)} className="p-4 bg-green-500 m-1 text-white rounded-full">Increment +</button>
+      <button onClick={() => setCount(count - 1)} className="p-4 bg-red-500 m-1  text-white rounded-full shadow-1xl ">Decrement -</button>
+    </>
+  );
 }
 
-export default Counter
+export default Counter;
